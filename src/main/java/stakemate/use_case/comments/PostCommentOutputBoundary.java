d18b64d8ecc0ee3bfded5b47c87b00e27485827d
Erwinnnnnnn <erzheng.zhang@mail.utoronto.ca>
2025-11-17 16:31:41 -0500

    Delete src/main/java/stakemate/use_case/comments directory

--- src/main/java/stakemate/use_case/comments/PostCommentOutputBoundary.java
+++
@@ -1,7 +0,0 @@
-package stakemate.use_case.comments;
-
-import stakemate.use_case.comments.PostCommentOutputData;
-
-public interface PostCommentOutputBoundary {
-    void present(PostCommentOutputData outputData);
-}
--- src/main/java/stakemate/use_case/comments/ViewCommentsOutputBoundary.java
+++
@@ -1,5 +0,0 @@
-package stakemate.use_case.comments;
-
-public interface ViewCommentsOutputBoundary {
-    void present(ViewCommentsOutputData outputData);
-}
